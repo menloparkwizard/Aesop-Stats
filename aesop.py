2569c597ce67122efaa1a8589551bb8313dc5fea
Kendrick Wiersma <menloparkwizard@gmail.com>
2016-12-05 23:14:19 -0700

    Output
    Add plotting algorithms

--- aesop.py
+++ aesop.py
@@ -7,6 +7,8 @@ import sys
 import codecs
 import glob
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
 # Local Imports
 from text import Text
@@ -27,11 +29,9 @@ for f in text_files:
   T = Text(f)
   T.parse_file()
   texts.append(T)
-  #stimulus.parse_file()
-  #stimulus.print_file_to_screen()
 
 wf_match = {}
-
+# wf_match_by_text = {}
 for p in wf_patterns:
   print "Searching for: " + p
   for t in texts:
@@ -40,12 +40,42 @@ for p in wf_patterns:
     else:
       wf_match[p] = t.find_word_final(p)
     print "Found: " + str(len(t.find_word_final(p))) + " matches in " + t.file()
+    # file = str(os.path.basename(t.file()))[:2]
+    # if file in wf_match_by_text:
+    #   wf_match_by_text[file].append({ p : len(t.find_word_final(p))})
+    # else:
+    #   wf_match_by_text[file] = [{ p : len(t.find_word_final(p))}]
+
+wf_match_by_text = pd.DataFrame(columns=wf_patterns)
+for t in texts:
+  counts = []
+  for p in wf_patterns:
+    counts.append(len(t.find_word_final(p)))
+  cs = pd.Series(counts, index=wf_patterns, name=str(os.path.basename(t.file()))[:2])
+  wf_match_by_text = wf_match_by_text.append(cs)
 
 
+#print wf_match_by_text
+
+word_final_counts = {}
 for p in wf_match:
-  print "Found: " + str(len(wf_match[p])) + " matches for " + p
-  #for t in wf_match[p]:
-   #print t
+  word_final_counts[p] = len(wf_match[p])
+  
 
-for i in wf_match[u"ф"]:
-  print i
+s = pd.Series(word_final_counts)
+#print "Word-Final Occurances:"
+#print s.to_string()
+plt.figure()
+s.plot(kind='bar')
+#print s
+#plt.bar(X, counts, facecolor='#9999ff', edgecolor='white')
+#plt.hist(counts, len(wf_patterns))
+#plt.show()
+plt.savefig("output/bar.png", dpi=800)
+
+print wf_match_by_text.to_string()
+plt.figure()
+wf_match_by_text.plot.box()
+plt.savefig("output/boxplot.png", dpi=800)
+#for i in wf_match[u"ф"]:
+#  print i
