a45b3b544b79c2ef7e9ac336ab33e0bcd8910761
Kendrick Wiersma <menloparkwizard@gmail.com>
2016-12-07 00:53:16 -0700

    Plot Axis
    Update plot axeses to display IPA characters instead of Russian orthography

--- aesop.py
+++ aesop.py
@@ -19,8 +19,10 @@ sys.stdout = codecs.getwriter('utf8')(sys.stdout)
 text_files = glob.glob("texts/*.txt")
 print text_files
 
-wf_patterns = [u"п", u"пь", u"б", u"бь", u"ф", u"фь", u"в", u"вь", u"т", u"ть", u"д", u"дь", u"с", u"сь", u"з", u"зь", u"ш", u"шь", u"ж", u"жь", u"к", u"г"]
+# , u"ж" u"ш",
 
+wf_patterns  = [u"п",  u"пь", u"б", u"бь", u"ф", u"фь", u"в", u"вь", u"т", u"ть", u"д", u"дь", u"с", u"сь", u"з", u"зь", u"шь?", u"жь?", u"к", u"г"]
+wf_ipa_equiv = [u"p ", u"pʲ", u"b", u"bʲ", u"f", u"pʲ", u"v", u"vʲ", u"t", u"tʲ", u"d", u"dʲ", u"s", u"sʲ", u"z", u"zʲ", u"ʂ",   u"ʐ",   u"k", u"ɡ"]
 print "Checking: " + str(len(wf_patterns)) + " patterns"
 
 # Parse all the text files
@@ -46,23 +48,23 @@ for p in wf_patterns:
     # else:
     #   wf_match_by_text[file] = [{ p : len(t.find_word_final(p))}]
 
-wf_match_by_text = pd.DataFrame(columns=wf_patterns)
+wf_match_by_text = pd.DataFrame(columns=wf_ipa_equiv)
 for t in texts:
   counts = []
   for p in wf_patterns:
     counts.append(len(t.find_word_final(p)))
-  cs = pd.Series(counts, index=wf_patterns, name=str(os.path.basename(t.file()))[:2])
+  cs = pd.Series(counts, index=wf_ipa_equiv, name=str(os.path.basename(t.file()))[:2])
   wf_match_by_text = wf_match_by_text.append(cs)
 
 
 #print wf_match_by_text
 
-word_final_counts = {}
+word_final_counts = []
 for p in wf_match:
-  word_final_counts[p] = len(wf_match[p])
+  word_final_counts.append(len(wf_match[p]))
   
 
-s = pd.Series(word_final_counts)
+s = pd.Series(word_final_counts, index=wf_ipa_equiv)
 #print "Word-Final Occurances:"
 #print s.to_string()
 plt.figure()
@@ -70,7 +72,7 @@ s.plot(kind='bar')
 #print s
 #plt.bar(X, counts, facecolor='#9999ff', edgecolor='white')
 #plt.hist(counts, len(wf_patterns))
-#plt.show()
+plt.show()
 plt.savefig("output/bar.png", dpi=800)
 
 print wf_match_by_text.to_string()
